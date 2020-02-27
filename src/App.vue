e0adddb57711db97003b577563e75c0a26e4e3bd
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-27 12:06:56 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="formatData()">Get Data</button>
+    <chart :generationMix="generationMix"></chart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chart from './components/Chart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'chart': Chart
+  },
+  data(){
+    return{
+      generationMix: [["fuel", "Percentage"]]
+    }
+  },
+  mounted(){
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(res => res.json())
+    .then(data => {
+      this.formatData(data.data.generationmix)
+      console.log(this.generationMix);
+    })
+  },
+  methods: {
+    formatData: function(energyData){
+      for (let energy of energyData) {
+        let energyArray = []
+        energyArray.push(energy.fuel);
+        energyArray.push(energy.perc)
+        this.generationMix.push(energyArray);
+      }
+
+      }
+    }
   }
-}
 </script>
 
 <style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="">
+    <!-- <p>{{generationMix}}</p> -->
+    <GChart
+    type="PieChart"
+    :data="generationMix"
+    />
+  </div>
+
+</template>
+
+<script>
+import {GChart} from "vue-google-charts";
+export default {
+  components: {
+    GChart
+  },
+  props: ['generationMix'],
+  data(){
+    return {
+      "dummyData": [['fuel', 'percentage'], ['biomass', 45], ['coal', 55]]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
